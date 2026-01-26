a9a7a57d23649b5bbedcfd850f0f511fbbda4f71
Goobus <huand2464@wrdsb.ca>
2026-01-26 12:00:11 -0500

    v0.0.3: Finished attacks (should work) and is_in_check

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,9 +4,10 @@
 #include "position.h"
 
 int main () {
-    Position pos;
+    Bitboards::init();
 
-    std::cout << pos.to_string();
+    Position pos("r1bqkbnr/pppp1Qpp/2n5/4p3/4P3/8/PPPP1PPP/RNB1KBNR b KQkq - 0 3");
+    std::cout << std::boolalpha << pos.is_square_attacked(E8, WHITE);
 
     return 0;
 }
